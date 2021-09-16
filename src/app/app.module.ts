428b8b30f0d51382f4b311807e381512bd64f555
AndrewHarper14 <aeh0031@mix.wvu.edu>
2021-09-16 17:43:22 -0400

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { LayoutMainComponent } from './layout/layout-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    LayoutMainComponent
   ],
   imports: [
     BrowserModule
